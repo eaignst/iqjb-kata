24c4253eac57231646bd456dacd53d9849e0b373
Dr. Vincze Attila <avincze73@gmail.com>
2019-07-18 15:35:00 +0200

    va

--- functional/src/main/java/hu/iqjb2/functional/Main.java
+++ functional/src/main/java/hu/iqjb2/functional/Main.java
@@ -5,6 +5,9 @@
  */
 package hu.iqjb2.functional;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author avincze
@@ -75,6 +78,41 @@ public class Main {
         System.out.println(
                 FruitFactory.INSTANCE.create("b").hashCode()
         );
-    }
+        
+        List<Integer> si = new ArrayList<>();
+        si.add(1);
+        si.add(2);
+        List<? extends Number> sn = si;
+        //sn.add(33);
+        sn.get(0);
+        
+        List<Number> sn2 = new ArrayList<>();
+        sn2.add(3.14);
+        sn2.add(3);
+        
+        List<? super Integer> si2 = sn2;
+        si2.get(7);
+        //si2.add(88.8);
+        
+        
+        
+        List<Object> objs = new ArrayList<Object>();
+        objs.add(1);
+        objs.add("two");
+        List<? super Integer> ints = objs;
+        ints.add(3); // ok
+        ints.get(0);
+        
+        
+        
+        
+        
+        
+
+}
+        
+        
+
+
     
 }
